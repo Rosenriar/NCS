d7a7fd9969b139a10c621dc85989a61bf3f7cfbc
admin <admin@DESKTOP-DDG5OFU>
2021-12-06 16:55:53 +0900

    123

--- DriverExample/src/DriverExample.java
+++ DriverExample/src/DriverExample.java
@@ -5,8 +5,25 @@ public class DriverExample {
       Driver driver=new Driver();
       bus bus = new bus();
       taxi taxi = new taxi();
+      Vehicle vh = new Vehicle();
       
       driver.driver(bus);
       driver.driver(taxi);
+      
+      if(bus instanceof bus) {
+    	  System.out.println("bus instanceof Bus.");
+      }else {
+    	  System.out.println("bus not instanceof Bus.");
+    	    }
+      if(bus instanceof Vehicle) {
+    	  System.out.println("bus instanceof Bus.");
+      }else {
+    	  System.out.println("bus not instanceof Bus.");
+      }
+      if(vh instanceof Vehicle) {
+    	  System.out.println("vh instanceof Bus.");
+      }else {
+    	  System.out.println("vh not instanceof Bus.");
    	}
    }
+}
